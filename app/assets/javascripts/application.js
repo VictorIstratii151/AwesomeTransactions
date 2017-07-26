c73b6081aaef3416936c2474122f696c2ea2b737
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-07-26 13:16:28 +0300

    Added bootstrap to project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootstrap/dropdown
+//= require turbolinks
