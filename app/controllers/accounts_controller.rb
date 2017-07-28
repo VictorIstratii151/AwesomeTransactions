93a3cfce6e6f532eea6edd76dbe785b7bdd7f389
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-07-28 16:13:14 +0300

    Made the accounts controller respond with json

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,9 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
+
+  respond_to :json
+
   def index
+    respond_with Account.all
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to accounts_path
-    end
   end
 end
