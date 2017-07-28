277e1fd7c400e6a41dff91107d8b1830cd3acccd
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-07-28 15:46:25 +0300

    scoped the resources to 'api'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
-  resources :accounts
+  scope "api" do
+    resources :accounts, defaults: { format: 'json' }
+  end
 
   root 'pages#index'
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
