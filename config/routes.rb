cdc6b72e4788c720350715279148ce773c7297a7
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-01 22:37:49 +0300

    matched the currencies index action to api scope

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get '/accounts', to: 'accounts#index'
+
+  get '/api/currencies', to: 'currencies#index'
+
+
+  get '/accounts/new', to: 'pages#index'
+
+  # match '*path', to: 'pages#index', via: [:get, :post]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
