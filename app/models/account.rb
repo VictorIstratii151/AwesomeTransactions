17afccfbbf6a5d095118c6a267ed402dbaa47042
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-05 22:10:12 +0300

    Added delete dependence in account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :user
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
   belongs_to :currency
 
   validates_presence_of :name, :currency, :balance
