8d624c2e0e9b68c79962b1ee4c12334ac5cc8637
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-09 00:13:25 +0300

    Removed the unnecessary rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
   scope "api" do
     resources :accounts, defaults: { format: 'json' }
+    resources :transactions, defaults: { format: 'json' }
   end
 
   root 'pages#index'
-
-  # get '/accounts', to: 'accounts#index'
-
   get '/api/currencies', to: 'currencies#index'
-
-
-  # get '/accounts/new', to: 'pages#index'
-
   get '/accounts/:id', to: 'pages#index'
-  # match '*path', to: 'pages#index', via: [:get, :post]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
