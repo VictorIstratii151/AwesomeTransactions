4b51875e601c0dd8d5a3d790b499f7c6e764d09e
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-10 22:25:50 +0300

    added confirmations to devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   get '/api/currencies', to: 'currencies#index'
   get '/accounts/:id', to: 'pages#index'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:confirmations => "confirmations"}
 end
