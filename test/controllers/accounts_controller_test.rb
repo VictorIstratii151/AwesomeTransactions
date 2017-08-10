ba9b38c6b1fd791f728b704bdffd793ec1c3df80
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-10 22:25:23 +0300

    Working on testing the controllers

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def sign_in(user)
+    post user_session_path \
+      "user[email]" => user.email,
+      "user[password]" => user.password
+  end
+
+  def setup
+    @user = User.create(email: "les@example.com", password: Devise::Encryptor.digest(User, "secret123"))
+    sign_in @user
+  end
+
+  test "should create account" do
+    assert_difference 'Account.count' do
+      post accounts_path,
+      params: {
+        account: {
+          name:     "test account",
+          currency_id: 1,
+          balance:  0.0
+        }
+      }
+    end
+  end
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creating a ghost account" do
+    account = Account.new
+    assert_not account.save, "Saved account with no name or smth"
+  end
 end
