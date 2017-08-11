87fa0052190e7b7ea29da400b0045d17a6c1ced3
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-11 18:27:07 +0300

    Defined a set of tests for my accounts controller

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,27 +1,71 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  def sign_in(user)
-    post user_session_path \
-      "user[email]" => user.email,
-      "user[password]" => user.password
+  def setup
+    @user = User.create(email: "les@example.com", password: "secret123")
+    sign_in @user
+
+    @account = @user.accounts.create(name: "random account", currency_id: currencies(:eur).id, balance: 0.0)
   end
 
-  def setup
-    @user = User.create(email: "les@example.com", password: Devise::Encryptor.digest(User, "secret123"))
-    sign_in @user
-  end
+
 
   test "should create account" do
-    assert_difference 'Account.count' do
+    assert_difference '@user.accounts.count' do
       post accounts_path,
-      params: {
-        account: {
-          name:     "test account",
-          currency_id: 1,
-          balance:  0.0
+        params: {
+          account: {
+            name:     "test account",
+            currency_id: currencies(:eur).id,
+            balance:  0.0
+          }
         }
-      }
+
+      assert_response :created
+    end
+
+    account = @user.accounts.last
+    assert_equal 'test account', account.name
+    assert_equal currencies(:eur), account.currency
+    assert_equal 0.0, account.balance
+  end
+
+
+  test "should return current user's accounts" do
+    get accounts_path
+    accounts = JSON.parse(response.body)
+
+    accounts.each do |account|
+      assert_equal account["user_id"], @user.id
     end
   end
+
+
+  test "should show account" do
+
+    get "/api/accounts/#{@account.id}"
+
+    assert_response :ok
+  end
+
+  test "should destroy account" do
+
+    assert_difference('@user.accounts.count', -1) do
+      delete "/api/accounts/#{@account.id}"
+
+      assert_response :no_content
+    end
+  end
+
+
+  test "should update account" do
+    patch "/api/accounts/#{@account.id}", params: { account: { name: "updated account name" } }
+
+    assert_response :no_content
+    # Reload association to fetch updated data and assert that name is updated.
+
+    @account.reload
+    assert_equal "updated account name", @account.name
+  end
+
 end
