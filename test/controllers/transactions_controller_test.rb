f35068b81be6cff1364b0589c6fd062d294dc492
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-11 19:11:28 +0300

    Defined the tests for transactions controller

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: "les@example.com", password: "secret123")
+    sign_in @user
+
+    @account = @user.accounts.create(name: "random account", currency_id: currencies(:eur).id, balance: 0.0)
+  end
+
+
+  test "should create a new transaction" do
+    assert_difference '@account.transactions.count' do
+      post transactions_path,
+        params: {
+          transaction: {
+            amount: 123.34,
+            description: "heh",
+            transaction_date: Time.now,
+            account_id: @account.id
+          }
+        }
+
+      assert_response :created
+    end
+
+    transaction = @account.transactions.last
+
+    assert_equal 123.34, transaction.amount
+    assert_equal "heh", transaction.description
+    assert_equal @account.id, transaction.account_id
+  end
+
+
+  test "should return all the transactions associated with account" do
+    get transactions_path
+
+    assert_response :ok
+  end
 end
