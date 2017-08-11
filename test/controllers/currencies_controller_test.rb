6b803fd020944a02edac9167fd973b3c64fa2e22
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-11 19:11:08 +0300

    Defined a test for the currencies controller index action

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CurrenciesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: "les@example.com", password: "secret123")
+    sign_in @user
+    @currencies = Currency.all
+  end
+
+  test "should return all the currencies" do
+    get '/api/currencies.json'
+    assert_response :ok
+  end
 end
