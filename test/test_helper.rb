18029140d8d15eb5c4519e0b40364b314a6ea857
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-08-11 19:12:13 +0300

    Moved the sign_in method to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+module SignInDevise
+  def sign_in(user)
+    post user_session_path \
+      "user[email]" => user.email,
+      "user[password]" => user.password
+  end
+end
+
+
 class ActiveSupport::TestCase
 
   # include Devise::Test::ControllerHelpers
@@ -9,5 +18,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInDevise
 end
 
